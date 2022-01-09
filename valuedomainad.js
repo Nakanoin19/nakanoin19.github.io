02065fbd7a3553a118bf754b13df0863ab541e8f
猫宮ぴあの / Piano Nekomiya <61345869+NSK-1010@users.noreply.github.com>
2022-01-09 16:34:24 +0900

    Update valuedomainad.js

--- valuedomainad.js
+++ valuedomainad.js
@@ -1,4 +1,4 @@
 var html = '<div id="valuedomainad" style="width:100%;"><iframe frameborder="0" style="border:none;width:100%;" src="https://nsk-1010.github.io/valuedomainad.html"></iframe></div>'
 document.querySelector("main#main").insertAdjacentHTML('beforebegin', html);
 // document.querySelector("#vdbanner").setAttribute("style", "display: none!important;");
-document.querySelector("body").insertBefore(document.querySelector('script[src="//cache1.value-domain.com/xrea_header.js"]'), document.querySelector("main#main"))
+document.querySelector("body").insertBefore(document.querySelector('script[src="//cache1.value-domain.com/xrea_header.js"]'), document.querySelector("main#main"));

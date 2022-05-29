156e5aa854bd039310a5dc03e265217dd44a84b2
猫宮ぴあの / Piano Nekomiya <kotone.olin1010@gmail.com>
2022-05-29 20:40:10 +0900

    a

--- valuedomainad.js
+++
@@ -1,4 +0,0 @@
-var html = '<div id="valuedomainad" style="width:100%;"><iframe frameborder="0" style="border:none;width:100%;" src="https://nsk-1010.github.io/valuedomainad.html"></iframe></div>'
-document.querySelector("main#main").insertAdjacentHTML('beforebegin', html);
-document.querySelector('script[src="//cache1.value-domain.com/xrea_header.js"]').remove();
-document.querySelector("#vdbanner").remove();
